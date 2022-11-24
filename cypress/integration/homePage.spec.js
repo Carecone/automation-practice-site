d92d62580a70446210e64e2294e5aa9fdf783af2
Diego Souza <souza.diegoty@gmail.com>
2022-11-24 00:47:20 -0300

    continuando os testes na página inicial

--- cypress/integration/homePage.spec.js
+++ cypress/integration/homePage.spec.js
@@ -3,10 +3,31 @@ describe('Tests Home page', () => {
         cy.visit('/')
     })
 
-    it('Home Page - with three Sliders only', () =>{
+    it('Home Page - with three Sliders only', () => {
         cy.shopMenuButton();
         cy.homeButton();
-        cy.countSliders();
+        cy.countElement('.n2-ss-slide-background-image', 3);
     })
 
+    it('Home page - with three Arrivals only', () => {
+        cy.shopMenuButton();
+        cy.homeButton();
+        cy.countElement('.attachment-shop_catalog', 3);
+    })
+
+    it('Home page - Images in Arrivals should navigate', () => {
+        cy.clickImage('title', 'Selenium Ruby');
+    })
+    
+    it('Home page - Arrivals-Images-Description', () =>{
+        cy.description('title', 'Thinking in HTML');
+    })
+
+    it('Home page - Arrivals-Images-Reviews', ()=>{
+        cy.review('title', 'Mastering JavaScript');
+    })
+
+    it('Home page - Arrivals-Images-Add to Basket', () =>{
+        cy.addBasket('title', 'Selenium Ruby', '500');
+    })
 })
--- cypress/support/gui_commands.js
+++ cypress/support/gui_commands.js
@@ -1,12 +1,51 @@
-Cypress.Commands.add('shopMenuButton', () =>{
+Cypress.Commands.add('shopMenuButton', () => {
     cy.get('a[id="menu-icon"]').click();
-    cy.contains('a', 'Shop').click();
+    cy.contains('a', 'Shop').should('be.visible').click();
+});
+
+Cypress.Commands.add('homeButton', () => {
+    cy.contains('a', 'Home').should('be.visible').click();
+});
+
+Cypress.Commands.add('countElement', (element, qtd) => {
+    cy.get(element).should('have.length', qtd).should('be.visible');
+});
+
+Cypress.Commands.add('clickImage', (type, text) => {
+    cy.get('a[id="menu-icon"]').should('be.visible').click();
+    cy.contains('a', 'Shop').should('be.visible').click();
+    cy.contains('a', 'Home').should('be.visible').click();
+    cy.get(`img[${type}="${text}"]`).should('be.visible').click();
+    cy.contains('h1', text).should('be.visible');
+});
+
+Cypress.Commands.add('description', (type, text) => {
+    cy.get('a[id="menu-icon"]').should('be.visible').click();
+    cy.contains('a', 'Shop').should('be.visible').click();
+    cy.contains('a', 'Home').should('be.visible').click();
+    cy.get(`img[${type}="${text}"]`).should('be.visible').click();
+    cy.contains('h1', text).should('be.visible');
+    cy.contains('a', 'Description').click()
+    cy.contains('h2', 'Product Description').should('be.visible');
 })
 
-Cypress.Commands.add('homeButton', () =>{
-    cy.contains('a', 'Home').click();
+Cypress.Commands.add('review', (type, text) => {
+    cy.get('a[id="menu-icon"]').should('be.visible').click();
+    cy.contains('a', 'Shop').should('be.visible').click();
+    cy.contains('a', 'Home').should('be.visible').click();
+    cy.get(`img[${type}="${text}"]`).should('be.visible').click();
+    cy.contains('h1', text).should('be.visible');
+    cy.contains('a', 'Reviews').click();
+    cy.contains('h2', 'Reviews').should('be.visible').click();
 })
 
-Cypress.Commands.add('countSliders', (name) =>{
-    cy.get('img', 'class=["n2-ss-slide-background-image n2-ss-slide-fill n2-ow"]').should('have.length', 3)
+Cypress.Commands.add('addBasket', (type, text, value) => {
+    cy.get('a[id="menu-icon"]').should('be.visible').click();
+    cy.contains('a', 'Shop').should('be.visible').click();
+    cy.contains('a', 'Home').should('be.visible').click();
+    cy.get(`img[${type}="${text}"]`).should('be.visible').click();
+    cy.contains('h1', text).should('be.visible');
+    cy.get('.single_add_to_cart_button').should('be.visible').click();
+    cy.get('a[id="menu-icon"]').click();
+    cy.get('.wpmenucart-contents').should('be.visible').should('have.text', `1 item₹${value}.00`);
 })
