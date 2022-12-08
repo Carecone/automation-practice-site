ae72c21975c884c35b9bc92fb8049b77ffcd728a
Diego Souza <souza.diegoty@gmail.com>
2022-12-07 22:57:20 -0300

    Adicinando todos os cenários do myAccount.spec.js

--- cypress/integration/myAccount.spec.js
+++ cypress/integration/myAccount.spec.js
@@ -3,7 +3,35 @@ describe('Tests My account', () =>{
         cy.visit('/');
     })
 
-    it('My Accounts-Dashboard', () =>{
+    it('My Accounts-Dashboard.', () =>{
+        
+    })
+
+    it('My Accounts-Orders.', () =>{
+
+    })
+
+    it('My Accounts-Orders.', () =>{
+
+    })
+
+    it('My Accounts-Orders.', () =>{
+
+    })
+
+    it('My Accounts-Address Functionality.', () =>{
+
+    })
+
+    it('My Accounts-Address Functionality.', () =>{
+
+    })
+
+    it('My Accounts-Account Details.', () =>{
+
+    })
+
+    it(' My Accounts-Log-Out.', () =>{
 
     })
 })
